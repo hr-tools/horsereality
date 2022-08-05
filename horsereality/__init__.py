49d1149025829ac0ce17030bf7113deee2ab0fbf
shay <bakkcat@gmail.com>
2022-08-05 16:22:12 +0200

    Bump version

--- horsereality/__init__.py
+++ horsereality/__init__.py
@@ -4,7 +4,7 @@ Simple client library for reading pages on Horse Reality.
 
 __author__ = 'shayypy'
 __copyright__ = 'Copyright shay (shayypy) 2022-present'
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 
 from .client import *
 from .enums import *
